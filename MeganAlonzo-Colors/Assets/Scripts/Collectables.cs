c036a34a27ea621ce824fddf98df30d854b5b04e
Megan Alonzo <Megan.Alonzo@iam.local>
2018-12-12 15:06:29 -0600

    adding scripts back

---
+++ MeganAlonzo-Colors/Assets/Scripts/Collectables.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+    private int coinCount;
+
+	void Start ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+	}
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("player"))
+        {
+            coinCount++;
+            Debug.Log("coin coint: " + coinCount);
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            Destroy(gameObject);
+        }
+    }
+}
